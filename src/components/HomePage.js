7a12a8dda08b2b7f417ae2c63bf5d0e867b93262
ZeusTheTrueGod <l-2732@yandex.ru>
2018-01-08 20:03:17 +0300

    playing with material-ui components

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from 'material-ui/Button';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Main App</h1>
+      <Button raised color="Primary">Hello</Button>
     </div>
   );
 };
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
+import Reboot from 'material-ui/Reboot';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './App';
 
 export default class Root extends Component {
@@ -8,9 +10,12 @@ export default class Root extends Component {
     const { store, history } = this.props;
     return (
       <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <App />
-        </ConnectedRouter>
+        <MuiThemeProvider>
+          <Reboot />
+          <ConnectedRouter history={history}>
+            <App />
+          </ConnectedRouter>
+        </MuiThemeProvider>
       </Provider>
     );
   }
