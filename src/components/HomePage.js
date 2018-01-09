9eff998b905e97254134a73599c64a988f0a8c7d
ZeusTheTrueGod <l-2732@yandex.ru>
2018-01-09 21:20:56 +0300

    even more components

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Button from 'material-ui/Button';
 import { FormGroup, FormControl, FormControlLabel, FormLabel } from 'material-ui/Form';
 import Checkbox from 'material-ui/Checkbox';
+import Select from 'material-ui/Select';
 import Radio, { RadioGroup } from 'material-ui/Radio';
+import Input  from 'material-ui/Input';
+import { MenuItem } from 'material-ui/Menu';
 
 const HomePage = () => {
   return (
     <div>
-      <Button raised color="Primary">Hello World</Button>
+      <Button raised primary>Hello World</Button>
       <br/>
       <br/>
       <FormGroup row>
@@ -49,6 +52,18 @@ const HomePage = () => {
 
       <FormGroup row>
         <FormControl component="fieldset">
+          <FormLabel component="legend">License</FormLabel>
+          <Select value="" input={<Input name="age" id="age-simple" />} >
+            <MenuItem value=""> Proprietary </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </FormGroup>
+
+      <FormGroup row>
+        <FormControl component="fieldset">
           <FormLabel component="legend">Market Cap of company</FormLabel>
             <RadioGroup name="stars" >
               <FormControlLabel value="any" control={<Radio />} label="any" />
@@ -60,6 +75,55 @@ const HomePage = () => {
             </RadioGroup>
         </FormControl>
       </FormGroup>
+
+      <FormGroup row>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">VC Funders</FormLabel>
+          <Select value={[]} multiple input={<Input name="age" id="age-simple" />} >
+            <MenuItem value=""> Funder 1 </MenuItem>
+            <MenuItem value={10}>Funder 2</MenuItem>
+            <MenuItem value={20}>Funder 3</MenuItem>
+            <MenuItem value={30}>Funder 4</MenuItem>
+          </Select>
+        </FormControl>
+      </FormGroup>
+
+      <FormGroup row>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Company</FormLabel>
+          <Select value={[]} multiple input={<Input name="age" id="age-simple" />} >
+            <MenuItem value="">Apple</MenuItem>
+            <MenuItem value={10}>M$</MenuItem>
+            <MenuItem value={20}>Linux Foundation</MenuItem>
+            <MenuItem value={30}>IBM</MenuItem>
+          </Select>
+        </FormControl>
+      </FormGroup>
+
+      <FormGroup row>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Headquaters Location</FormLabel>
+          <Select value="" input={<Input name="age" id="age-simple" />} >
+            <MenuItem value="">NY</MenuItem>
+            <MenuItem value={10}>San FranciscoM$</MenuItem>
+            <MenuItem value={20}>Chicago</MenuItem>
+            <MenuItem value={30}>Denver</MenuItem>
+          </Select>
+        </FormControl>
+      </FormGroup>
+
+      <FormGroup row>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">LandScape Section</FormLabel>
+            <RadioGroup name="stars" >
+              <FormControlLabel value="any" control={<Radio />} label="any" />
+              <FormControlLabel value="male" control={<Radio />} label="logging" />
+              <FormControlLabel value="female" control={<Radio />} label="monitoring" />
+            </RadioGroup>
+        </FormControl>
+      </FormGroup>
+
+
 
 
     </div>
