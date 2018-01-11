900ea10d529a9219d00276266ff6a5ba71029788
ZeusTheTrueGod <l-2732@yandex.ru>
2018-01-11 22:00:00 +0300

    initial layout test

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,15 +3,24 @@ import Filters from './Filters';
 import Grouping from './Grouping';
 import Sorting from './Sorting';
 import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+
 
 const HomePage = () => {
   return (
     <div>
-      <Button raised primary>Hello World</Button>
-      <br/>
-      <Filters/>
-      <Grouping/>
-      <Sorting/>
+      <Grid container spacing={24}>
+        <Grid item xs={3} sm={6}>
+          <Filters/>
+          <Grouping/>
+          <Sorting/>
+        </Grid>
+        <Grid item xs={9} sm={6}>
+          <h1> Main Content </h1>
+          <Button raised primary>Hello World</Button>
+        </Grid>
+      </Grid>
     </div>
   );
 };
