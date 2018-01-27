cb1ffd58ffa17d0905fd30c0ebbd936a74897bef
gcid12 <gcid12@gmail.com>
2018-01-26 23:10:22 -0500

    Adjust mosaic background image

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import { GridListTileBar } from 'material-ui/GridList';
 import Subheader from 'material-ui/List/ListSubheader';
@@ -7,12 +8,12 @@ import _ from 'lodash';
 const MainContent = ({groupedItems, onSelectItem}) => {
   const itemsAndHeaders = _.map(groupedItems, function(groupedItem) {
     return [
-      <div key={"subheader:" + groupedItem.header} style={{position: 'relative', height: 80, margin: 20}}>
+      <div className="sh_wrapper" key={"subheader:" + groupedItem.header}>
           <Subheader component="div" style={{fontSize: 24}}>{groupedItem.header} ({groupedItem.items.length})</Subheader>
       </div>
     ].concat(_.map(groupedItem.items, function(item) {
       return (<div className="mosaic" key={item.id}>
-                <div className={item.logo} onClick={() => onSelectItem(item.id)}/>
+                <div className={classNames(`${item.logo}`,{ logo: true})} onClick={() => onSelectItem(item.id)}/>
                 <GridListTileBar
                   title={item.name}
                   subtitle={<div><span>{item.company} (${item.marketCap}M)</span> | <span>⭐️ {item.stars}</span></div>}
