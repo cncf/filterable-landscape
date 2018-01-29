e8c050c93f9b3fdff62ffb74e3d732c8620a9c30
gcid12 <gcid12@gmail.com>
2018-01-28 21:55:40 -0800

    Merge

---
+++ src/components/HeaderContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeaderContainer = () => {
+
+  return (
+    <div style={{width:'100%', backgroundColor:'#CCC'}}>
+      <h1>Cloud Native Landscape</h1>
+      <div>v0.2</div>
+    </div>
+  );
+};
+
+export default HeaderContainer;
