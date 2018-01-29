dafa3939ed653d00eda3327c665ae0e006be8997
gcid12 <gcid12@gmail.com>
2018-01-28 20:19:29 -0800

    Add logo responsiveness, add missing oss if

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,14 +3,16 @@ import classNames from 'classnames';
 import Icon from 'material-ui/Icon';
 import Subheader from 'material-ui/List/ListSubheader';
 import _ from 'lodash';
+import formatAmount from '../utils/formatAmount';
 
-//import formatNumber from 'format-number';
-// const formatStars = function(v) {
-//   if (_.isNumber(v)) {
-//     return  formatNumber({integerSeparator: ','})(v);
-//   }
-//   return v;
-// };
+
+import formatNumber from 'format-number';
+const formatStars = function(v) {
+  if (_.isNumber(v)) {
+    return  formatNumber({integerSeparator: ','})(v);
+  }
+  return v;
+};
 
 const MainContent = ({groupedItems, onSelectItem}) => {
   const itemsAndHeaders = _.map(groupedItems, function(groupedItem) {
@@ -20,14 +22,17 @@ const MainContent = ({groupedItems, onSelectItem}) => {
       </div>
     ].concat(_.map(groupedItem.items, function(item) {
       return (<div className="mosaic" key={item.id}>
-                <div className={classNames(`${item.logo}`,{ logo: true})} onClick={() => onSelectItem(item.id)}/>
+                <div className="logo_wrapper" style={{border: '1px solid #f0f0f0', background: item.oss ? '' : '#eeeeee'}}
+                >
+                  <div className={classNames(`${item.logo}`,{ logo: true})} onClick={() => onSelectItem(item.id)}/>
+                </div>
                 <div className="info">
                   <h5>{item.name}</h5>
                   <div>
-                    <span>{item.company} (${item.marketCap}M)</span>
+                    <span>{item.company} ({formatAmount(item.marketCap)})</span>
                     <span>
                       <Icon color="disabled" style={{ fontSize: 15 }}>star</Icon>
-                      <span>{item.stars}</span>
+                      <span>{formatStars(item.stars)}</span>
                     </span>
                   </div>
                 </div>
