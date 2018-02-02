89f84aad15adf086dae6618aa4d937a4de1d1f06
gcid12 <gcid12@gmail.com>
2018-02-02 09:18:04 -0800

    Header Container Markup

---
+++ src/components/HeaderContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const HeaderContainer = () => {
+  
+  return (
+    <div className="header_container">
+      <div className="title">
+        
+        <p>1</p>
+      </div>
+      
+      <div className="header">
+        <div className="logos">
+          <a href="https://www.cncf.io/" target="_blank" rel="noopener noreferrer">
+            <div className="partner1" />
+          </a>
+        </div>
+      
+      </div>
+    </div>
+  );
+};
+
+export default HeaderContainer;
