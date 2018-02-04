c449a1a2c75815b5eff315bda08a81ed8ee07ea0
Dan Kohn <dan@dankohn.com>
2018-02-04 14:16:16 -0500

    Change info note
    Signed-off-by: Dan Kohn <dan@dankohn.com>

--- src/components/HeaderContainer.js
+++ src/components/HeaderContainer.js
@@ -6,12 +6,13 @@ const HeaderContainer = () => {
     <div className="header_container">
       <div className="header">
         <div className="logos">
-          <a href="" target="_blank" rel="noopener noreferrer">
             <div className="cncf_landscape" />
-          </a>
-          <div className="info"><h4>
-              This is the interactive counterpart to <a href="https://github.com/cncf/landscape#current-version">CNCF's Cloud Native Landscape</a>. Please report any issues or, even better, open a pull request.
-          </h4></div>
+          <div className="info"><p>
+              This is the interactive counterpart to CNCF's Cloud Native
+              <a href="https://github.com/cncf/landscape#current-version">Landscape</a>.
+              Please <a href="https://github.com/cncf/filterable-landscape/issues/new/">report</a> any issues or,
+              even better, open a pull request.
+          </p></div>
           <a href="https://www.cncf.io/" target="_blank" rel="noopener noreferrer">
             <div className="cncf_logo" />
           </a>
