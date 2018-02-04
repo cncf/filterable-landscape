4db5d75efdae29942d5e46a00e364013f17bea29
ZeusTheTrueGod <l-2732@yandex.ru>
2018-02-04 21:25:08 +0300

    cleaning up the page. closes #100

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,6 @@ import Filters from './Filters';
 import Grouping from './Grouping';
 import Sorting from './Sorting';
 import MainContentContainer from './MainContentContainer';
-import LeftPanelInfoContainer from './LeftPanelInfoContainer';
 import HomePageUrlContainer from './HomePageUrlContainer';
 import ResetFiltersContainer from './ResetFiltersContainer';
 import ItemDialogContainer from './ItemDialogContainer';
@@ -24,14 +23,12 @@ const HomePage = ({hasData}) => {
       <Grid container spacing={24}>
         <Grid item xs={3} sm={3} className="sidebar">
           <ResetFiltersContainer />
-          <Filters />
           <Grouping/>
           <Sorting/>
-          <LeftPanelInfoContainer style={{width: 200}}/>
+          <div style={{width: 200, height: 30}}/>
+          <Filters />
         </Grid>
         <Grid item xs={9} sm={9} className="content">
-          <h1 className='logo'> Summary Placeholder </h1>
-          <h1> Available Presets Placeholder</h1>
           <MainContentContainer/>
         </Grid>
       </Grid>
