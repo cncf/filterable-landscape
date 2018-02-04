d24772867023727b7177758c0682cf49d10ff1ad
gcid12 <gcid12@gmail.com>
2018-02-04 10:52:30 -0800

    Reset icon

--- src/components/ResetFilters.js
+++ src/components/ResetFilters.js
@@ -6,7 +6,7 @@ const Filters = ({reset}) => {
   return (
     <div className="filter_btn">
     <IconButton style={{ fontSize:'0.8em', width:'100px' }} onClick={()=>reset()} aria-label="Reset Filters">
-      <Icon>alarm</Icon> Reset Filters
+      <Icon>settings_backup_restore</Icon> Reset Filters
     </IconButton>
     
     </div>
