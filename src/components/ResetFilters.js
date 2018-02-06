5a2c16abf633baa5d7d3db5313c9fe279bdd0aae
gcid12 <gcid12@gmail.com>
2018-02-05 23:24:35 -0800

    clean up

--- src/components/ResetFilters.js
+++ src/components/ResetFilters.js
@@ -4,7 +4,7 @@ import IconButton from 'material-ui/IconButton';
 
 const Filters = ({reset}) => {
   return (
-    <IconButton disableRipple style={{ color:'#4567d8', fontSize:'0.8em', width:'100%', justifyContent:'flex-start' }} onClick={()=>reset()} aria-label="Reset Filters">
+    <IconButton disableRipple style={{ color:'#366fa8', fontSize:'0.8em', width:'100%', justifyContent:'flex-start' }} onClick={()=>reset()} aria-label="Reset Filters">
       <Icon style={{ fontSize:'1.2em'}}>settings_backup_restore</Icon> Reset Filters
     </IconButton>
   );
