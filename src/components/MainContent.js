038a72427e385c5e39af54b300df239743da3a54
gcid12 <gcid12@gmail.com>
2018-02-07 08:36:30 -0800

    Logic to detect relation

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Icon from 'material-ui/Icon';
 import millify from 'millify'
+import classNames from 'classnames'
 import Subheader from 'material-ui/List/ListSubheader';
 import _ from 'lodash';
 
@@ -11,7 +12,9 @@ const MainContent = ({groupedItems, onSelectItem}) => {
           <Subheader component="div" style={{fontSize: 24}}>{groupedItem.header} ({groupedItem.items.length})</Subheader>
       </div>
     ].concat(_.map(groupedItem.items, function(item) {
-      return (<div className="mosaic" key={item.id} style={{ background: item.oss ? 'white' : '#eeeeee'}} onClick={() => onSelectItem(item.id)} >
+      return (<div className={classNames('mosaic',{inception : item.cncfRelation ==='inception'}, {incubating : item.cncfRelation ==='incubating'}, {graduated : item.cncfRelation ==='graduated'})}
+                   key={item.id} style={{ background: item.oss ? 'white' : '#eeeeee'}}
+                   onClick={() => onSelectItem(item.id)} >
                 <div className="logo_wrapper" style={{border: '1px solid #f0f0f0'}}>
                   <img src={item.hrefTile} className='logo' max-height='100%' max-width='100%' />
                 </div>
