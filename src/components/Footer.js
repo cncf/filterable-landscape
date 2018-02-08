b796eb4618152de428f0749182732c4a33d3c3d8
Dan Kohn <dan@dankohn.com>
2018-02-08 12:04:21 -0500

    Fix link
    Signed-off-by: Dan Kohn <dan@dankohn.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return <div style={{ marginTop: 10, fontSize:'9pt', width: '100%', textAlign: 'center' }}>
-    Crunchbase data is used under license from Crunchbase to CNCF. For more information, please see <a href="https://data.crunchbase.com/v3.1/docs/terms.">terms</a>
+    Crunchbase data is used under license from Crunchbase to CNCF. For more information, please see <a href="https://data.crunchbase.com/v3.1/docs/terms">terms</a>.
   </div>
 }
 export default Footer;
