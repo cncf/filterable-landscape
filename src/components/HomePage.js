bb380f90c60e6a9b6d942a297dc819d6ce1bd208
gcid12 <gcid12@gmail.com>
2018-02-09 07:37:06 -0800

    Revert to stateless

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import { withStyles } from 'material-ui/styles';
-import Drawer from 'material-ui/Drawer';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import IconButton from 'material-ui/IconButton';
-import Icon from 'material-ui/Icon';
-import Hidden from 'material-ui/Hidden';
 import Grid from 'material-ui/Grid';
-
+import StickyBox from "react-sticky-box";
 import Filters from './Filters';
 import Grouping from './Grouping';
 import Sorting from './Sorting';
@@ -18,147 +10,38 @@ import ResetFiltersContainer from './ResetFiltersContainer';
 import ItemDialogContainer from './ItemDialogContainer';
 import HeaderContainer from './HeaderContainer';
 
-const drawerWidth = 240;
-
-const styles = theme => ({
-  root: {
-    width: '100%',
-    height: 430,
-    marginTop: theme.spacing.unit * 3,
-    zIndex: 1,
-    overflow: 'hidden',
-  },
-  appFrame: {
-    position: 'relative',
-    display: 'flex',
-    width: '100%',
-    height: '100%',
-  },
-  appBar: {
-    position: 'absolute',
-    marginLeft: drawerWidth,
-    [theme.breakpoints.up('md')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-    },
-  },
-  navIconHide: {
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
-  drawerHeader: theme.mixins.toolbar,
-  drawerPaper: {
-    width: 250,
-    [theme.breakpoints.up('md')]: {
-      width: drawerWidth,
-      position: 'relative',
-      height: '100%',
-    },
-  },
-  content: {
-    backgroundColor: theme.palette.background.default,
-    width: '100%',
-    padding: theme.spacing.unit * 3,
-    height: 'calc(100% - 56px)',
-    marginTop: 56,
-    [theme.breakpoints.up('sm')]: {
-      height: 'calc(100% - 64px)',
-      marginTop: 64,
-    },
-  },
-});
-
-class HomePage extends React.Component {
-  state = {
-    mobileOpen: false,
-  };
-  
-  handleDrawerToggle = () => {
-    this.setState({ mobileOpen: !this.state.mobileOpen });
-  };
-  
-  render() {
-    const { classes, theme } = this.props;
-    
-    
-    const drawer =(
-      <div className="sidebar">
-        <div className="content-sidebar">
-          <ResetFiltersContainer />
-          <Grouping/>
-          <Sorting/>
-          <div style={{width: 200, height: 30}}/>
-          <Filters />
-        </div>
+const HomePage = ({hasData}) => {
+  if (!hasData) {
+    return <h1>Loading data... </h1>;
+  }
+  return (
+      <div>
+        <HomePageUrlContainer />
+        <ItemDialogContainer />
+        <HeaderContainer/>
       
-      </div>
-    );
-    
-    return (
-      <div className={classes.root}>
-        <div className={classes.appFrame}>
-          <AppBar className={classes.appBar}>
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={this.handleDrawerToggle}
-                className={classes.navIconHide}
-              >
-                <Icon>menu</Icon>
-              </IconButton>
-              <Typography variant="title" color="inherit" noWrap>
-                Responsive drawer
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Hidden mdUp>
-            <Drawer
-              variant="temporary"
-              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-              open={this.state.mobileOpen}
-              classes={{
-                paper: classes.drawerPaper,
-              }}
-              onClose={this.handleDrawerToggle}
-              ModalProps={{
-                keepMounted: true,
-              }}
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
-          <Hidden smDown implementation="css">
-            <Drawer
-              variant="permanent"
-              open
-              classes={{
-                paper: classes.drawerPaper,
-              }}
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
-          <main className={classes.content}>
-            
-            <HomePageUrlContainer />
-            <ItemDialogContainer />
-            <HeaderContainer/>
-            
-            <Grid item xs={12} sm={12} className="content">
-              <div className="disclaimer">
-                This is the interactive counterpart to CNCF's Cloud Native <a href="https://github.com/cncf/landscape#current-version">Landscape</a>.
+        <Grid container spacing={24}>
+          <Grid item xs={2} sm={2} className="sidebar">
+            <StickyBox className="content-sidebar">
+              <ResetFiltersContainer />
+              <Grouping/>
+              <Sorting/>
+              <div style={{width: 200, height: 30}}/>
+              <Filters />
+            </StickyBox>
+
+          </Grid>
+          <Grid item xs={10} sm={10} className="content">
+            <div className="disclaimer">
+                This is the interactive counterpart to CNCF&#180;s Cloud Native <a href="https://github.com/cncf/landscape#current-version">Landscape</a>.
                 Please <a href="https://github.com/cncf/filterable-landscape/issues/new/">report</a> any issues or,
                 even better, open a pull request.
-              </div>
-              <MainContentContainer/>
-            </Grid>
-          </main>
-        </div>
+            </div>
+            <MainContentContainer/>
+          </Grid>
+        </Grid>
       </div>
-    );
-  }
-}
+  );
+};
 
-
-export default withStyles(styles, { withTheme: true })(HomePage);
+export default HomePage;
