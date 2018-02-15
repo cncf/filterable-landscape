c61d19c8f66072fbac0a53bd6e8690f29a82b432
alexkontsevoy <akontsevoy@ronasit.com>
2018-02-15 23:20:31 +0600

    sag as references

--- src/components/HeaderContainer.js
+++ src/components/HeaderContainer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import landscapeLogo from '../styles/assets/cncf-landscape.svg';
+import cncfLogo from '../styles/assets/cncf.svg';
 
 const HeaderContainer = (props) => {
 
@@ -7,9 +9,9 @@ const HeaderContainer = (props) => {
       <div className="header">
         
           {props.children}
-          <span className="landscape-logo"><img src="/styles/assets/cncf-landscape.svg" /></span>
+          <span className="landscape-logo"><img src={landscapeLogo} /></span>
           <a href="https://www.cncf.io/" target="_blank" rel="noopener noreferrer" className="cncf-logo">
-          	<img src="/styles/assets/cncf.svg" />
+          	<img src={cncfLogo} />
           </a>
 
       </div>
