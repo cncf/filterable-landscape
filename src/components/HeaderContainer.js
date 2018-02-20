c245975ea4e845f06260ac6c59167cc804775cab
ZeusTheTrueGod <l-2732@yandex.ru>
2018-02-20 23:52:29 +0300

    all external links report to Google. #42

--- src/components/HeaderContainer.js
+++ src/components/HeaderContainer.js
@@ -1,15 +1,16 @@
 import React from 'react';
-const HeaderContainer = (props) => {
+import { OutboundLink } from 'react-ga';
 
+const HeaderContainer = (props) => {
   return (
     <div className="header_container">
       <div className="header">
-        
+
           {props.children}
           <span className="landscape-logo"><img src="/images/cncf-landscape.svg" /></span>
-          <a href="https://www.cncf.io/" target="_blank" rel="noopener noreferrer" className="cncf-logo">
-          	<img src="/images/cncf.svg" />
-          </a>
+          <OutboundLink eventLabel="cncf" to="https://www.cncf.io/" target="_blank" rel="noopener noreferrer" className="cncf-logo">
+    <img src="/images/cncf.svg" />
+  </OutboundLink>
 
       </div>
     </div>
