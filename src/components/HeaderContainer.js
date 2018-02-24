3916a655ea2e980a5e24336e5c3a00bdae2ba746
ZeusTheTrueGod <l-2732@yandex.ru>
2018-02-25 00:23:28 +0300

    handle iPhone dialogs in a special way

--- src/components/HeaderContainer.js
+++ src/components/HeaderContainer.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { OutboundLink } from 'react-ga';
 
-const HeaderContainer = (props) => {
+const HeaderContainer = () => {
   return (
     <div className="header_container">
       <div className="header">
-
-          {props.children}
           <span className="landscape-logo"><img src="/images/cncf-landscape.svg" /></span>
           <OutboundLink eventLabel="cncf" to="https://www.cncf.io/" target="_blank" rel="noopener noreferrer" className="cncf-logo">
     <img src="/images/cncf.svg" />
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,8 +15,13 @@ import HeaderContainer from './HeaderContainer';
 import SummaryContainer from './SummaryContainer';
 import Footer from './Footer';
 
+import isIphone from '../utils/isIphone';
 
-const HomePage = ({ready, filtersVisible, hideFilters, showFilters}) => {
+const state = {
+  lastScrollPosition: 0
+};
+
+const HomePage = ({ready, hasSelectedItem, filtersVisible, hideFilters, showFilters, onClose}) => {
   if (!ready) {
     return (
       <div>
@@ -24,42 +29,75 @@ const HomePage = ({ready, filtersVisible, hideFilters, showFilters}) => {
       </div>
     )
   }
+  // if (isIphone) {
+    // return(
+      // <div className="app background">
+        // <div className="iphoneScroller">
+          // <HeaderContainer/>
+          // <IconButton className="sidebar-show" onClick={showFilters}><Icon>menu</Icon></IconButton>
+          // <div className="1shadow"/>
+          // <div className="container" />
+          // <div className="app-overlay" onClick={hideFilters}></div>
+          // <HomePageUrlContainer />
+          // <div className="main">
+            // <div className="disclaimer">
+              // <h1>CNCF Cloud Native Interactive Landscape</h1>
+              // This is the interactive counterpart to CNCF's Cloud Native <a href="https://github.com/cncf/landscape#current-version">Landscape</a>.
+              // Please <a href="https://github.com/cncf/filterable-landscape/issues/new/">report</a> any issues or,
+              // even better, open a pull request. Greyed logos are not open source.
+            // </div>
+            // <SummaryContainer />
+            // <MainContentContainer/>
+            // <Footer/>
+          // </div>
+        // </div>
+      // </div>);
+  // }
+
+  if (isIphone && hasSelectedItem) {
+    //try to get a current scroll if we are in a normal mode
+    if (!document.querySelector('iphone-scroller')) {
+      state.lastScrollPosition = document.scrollingElement.scrollTop;
+    }
+  }
 
   return (
-    <div className={classNames('app',{'filters-opened' : filtersVisible})}>
-
-      <HeaderContainer/>
-      <IconButton className="sidebar-show" onClick={showFilters}><Icon>menu</Icon></IconButton>
-
-      <div className="sidebar">
-        <IconButton className="sidebar-collapse" onClick={hideFilters}><Icon>close</Icon></IconButton>
-        <ResetFiltersContainer />
-        <Grouping/>
-        <Sorting/>
-        <Filters />
-        <Presets />
-        <Ad />
-      </div>
-
-      <div className="app-overlay" onClick={hideFilters}></div>
-
-      <HomePageUrlContainer />
-      <ItemDialogContainer />
-
-      <div className="main">
-        <div className="disclaimer">
-          <h1>CNCF Cloud Native Interactive Landscape</h1>
-          This is the interactive counterpart to CNCF's Cloud Native <a href="https://github.com/cncf/landscape#current-version">Landscape</a>.
-          Please <a href="https://github.com/cncf/filterable-landscape/issues/new/">report</a> any issues or,
-          even better, open a pull request. Greyed logos are not open source.
+    <div>
+    <ItemDialogContainer/>
+    <div className={classNames('app',{'filters-opened' : filtersVisible, 'background': isIphone && hasSelectedItem})}>
+      <dic className={classNames({"shadow": isIphone && hasSelectedItem})} onClick={onClose} />
+      <div style={{marginTop: -state.lastScrollPosition}} className={classNames({"iphone-scroller": isIphone && hasSelectedItem})} >
+        <HeaderContainer/>
+        <IconButton className="sidebar-show" onClick={showFilters}><Icon>menu</Icon></IconButton>
+        <div className="sidebar">
+          <IconButton className="sidebar-collapse" onClick={hideFilters}><Icon>close</Icon></IconButton>
+          <ResetFiltersContainer />
+          <Grouping/>
+          <Sorting/>
+          <Filters />
+          <Presets />
+          <Ad />
         </div>
 
-        <SummaryContainer />
-        <MainContentContainer/>
-        <Footer/>
+        <div className="app-overlay" onClick={hideFilters}></div>
+
+        <HomePageUrlContainer />
+        { /*<ItemDialogContainer />*/}
+
+        <div className="main">
+          <div className="disclaimer">
+            <h1>CNCF Cloud Native Interactive Landscape</h1>
+            This is the interactive counterpart to CNCF's Cloud Native <a href="https://github.com/cncf/landscape#current-version">Landscape</a>.
+            Please <a href="https://github.com/cncf/filterable-landscape/issues/new/">report</a> any issues or,
+            even better, open a pull request. Greyed logos are not open source.
+          </div>
+
+          <SummaryContainer />
+          <MainContentContainer/>
+          <Footer/>
+        </div>
       </div>
-
-
+    </div>
     </div>
   );
 };
